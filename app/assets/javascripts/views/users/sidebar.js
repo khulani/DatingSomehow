3ad8e92b4b17a2406a44cf9a0cde5d875c084000
Khulani Malone <khulani@gmail.com>
2015-02-19 00:44:17 -0800

    undid login redirect changes

--- app/assets/javascripts/views/users/sidebar.js
+++ app/assets/javascripts/views/users/sidebar.js
@@ -15,8 +15,6 @@ ActoExplaino.Views.Sidebar = Backbone.View.extend({
   checkUser: function () {
     if (this.personal && !this.model.id) {
       this.personal = false;
-    } else {
-      this.personal = true;
     }
     this.render();
   },
